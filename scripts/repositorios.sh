19a1c57eef198159c291134266953ba6decda324
marionumza <marionumza@gmail.com>
2018-09-18 20:52:09 -0300

    m

---
+++ scripts/repositorios.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+cd ..
+cd data/custom-addons/
+git clone https://github.com/ju-san/report_xls.git
+git clone -b 9.0 https://github.com/it-projects-llc/pos-addons.git
+git clone -b 9.0 https://github.com/it-projects-llc/misc-addons.git
+git clone -b 9.0 https://github.com/CybroOdoo/CybroAddons.git
+git clone -b 9.0 https://github.com/it-projects-llc/access-addons.git
+git clone -b 9.0 https://github.com/marionumza/artarad_theme_v9.git
+git clone -b 9.0 https://github.com/JayVora-SerpentCS/SerpentCS_Contributions.git
+
